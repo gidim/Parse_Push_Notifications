388730ae3202b06637826d923c33e6146587c19a
Gideon Mendels <gideonm@gmail.com>
2015-12-08 14:57:40 -0500

    converted to maven

--- src/main/java/th/in/santi/parse/Example.java
+++ src/main/java/th/in/santi/parse/Example.java
@@ -1,3 +1,5 @@
+package th.in.santi.parse;
+
 import java.util.Date;
 
 import th.in.santi.parse.Parse;
@@ -9,10 +11,10 @@ import th.in.santi.parse.constants.ParseConstants;
  */
 public class Example {
     public static void main(String[] args) {
-        Parse.initialize(Application_ID, REST_API_Key); //menu --> Settings >> Keys 
+        Parse.initialize("",""); //menu --> Settings >> Keys
         ParsePush p = new ParsePush();
-        p.setMessage("Hello World");
-        p.setObjectId("123"); //menu --> Core
+        p.setMessage("Let me know if you got it!");
+        p.setDeviceToken("b0063c43bf78038c83e57262b177dfe9b35197658b9382ab69a7143047deddba");
         p.send();
     }
 }
